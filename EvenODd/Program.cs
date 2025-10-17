de80ea8ce5a4aed4445d05060bcabf810efdea7c
ramya <ramya@RAMYA>
2025-10-17 10:32:03 +0530

    Add project files.

---
+++ EvenODd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenODd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if(a%2==0)
+            {
+                Console.WriteLine("Even Number");
+
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+
+
+        }
+    }
+}
